b80677f8ca5ef06d49c024693f219b0396f2a944
bvanbeynum <maildrop444@gmail.com>
2017-07-06 08:56:07 -0400

    Update static.js

--- server/static.js
+++ server/static.js
@@ -81,7 +81,7 @@ module.exports = function (app) {
 	});
 	
 	app.get("/test", function(request, response) {
-		response.send("version: 10");
+		response.send("version: 11");
 	});
 	
 	app.get("*", function (request, response) {
